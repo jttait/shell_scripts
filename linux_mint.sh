d71e66d4fbe2fb0321249e6d544184b962771544
Jonathon Tait <jttait@gmail.com>
2026-01-11 13:28:20 +0000

    Remove unused directories

--- linux_mint.sh
+++ linux_mint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+rm --recursive --force ~/Music
+rm --recursive --force ~/Videos
+rm --recursive --force ~/Pictures
+rm --recursive --force ~/Public
+rm --recursive --force ~/Templates
+rm --recursive --force ~/Documents
+
 sudo apt install git
 sudo apt install vim
 sudo apt install borgbackup
