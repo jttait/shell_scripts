73a4fde83ce722dfc8a74adb402be490d9fbb1dc
Jonathon Tait <jttait@gmail.com>
2026-02-07 08:59:28 +0000

    Fix typo in paths

--- test/testBorgDrive.sh
+++ test/testBorgDrive.sh
@@ -12,9 +12,9 @@ throwExceptionIfDirectoriesDifferent() {
 }
 
 # setup
-echo '#!/bin/bash' >> secrets.sh
+echo '#!/bin/bash' >> ../secrets.sh
 echo 'PATHS_TO_BACKUP="./directory ./another_directory ./file.txt' >> ../secrets.sh
-echo 'PATH_TO_BORG_DRIVE_REPO=./repo' >> secrets.sh
+echo 'PATH_TO_BORG_DRIVE_REPO=./repo' >> ../secrets.sh
 mkdir -p directory
 mkdir -p another_directory
 echo "hello, world" >> file.txt
